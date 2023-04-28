386c2e74d97c57f3971960025e6d39b679936931
bulatkhu <hudainazarov.b@gmail.com>
2023-04-28 14:57:04 +0200

    Printing most expensive book

--- src/übung2/WebshopTest.java
+++ src/übung2/WebshopTest.java
@@ -7,5 +7,13 @@ public class WebshopTest {
         System.out.println(b1.getBeschreibung());
         System.out.println("Preis: " + b1.getPreis() + " Euro");
         System.out.println("Barcode: " + b1.getBarcode());
+
+
+        Buch b2 = new Buch("UML 3.0", 12, 9783897215214L,
+                139, "Dan Pilone", 2);
+
+        String MostExpenciveBookDescription = b1.getPreis() < b2.getPreis() ? b2.getBeschreibung() : b1.getBeschreibung();
+
+        System.out.printf("The more expensive book is: %s%n", MostExpenciveBookDescription);
     }
 }
